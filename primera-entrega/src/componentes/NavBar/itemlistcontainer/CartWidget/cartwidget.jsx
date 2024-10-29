137804fef0d57aa386a9bafcaaa7f1636c74078f
Dante <dantrux22@gmail.com>
2024-10-29 11:37:14 -0300

    .

--- primera-entrega/src/componentes/NavBar/itemlistcontainer/CartWidget/cartwidget.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function cartwidget() {
-  return (
-    <div>cartwidget</div>
-  )
-}
-
-export default cartwidget
--- primera-entrega/src/componentes/NavBar/itemlistcontainer/itemlistcontainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function itemlistcontainer() {
-  return (
-    <div>itemlistcontainer</div>
-  )
-}
-
-export default itemlistcontainer
